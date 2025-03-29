ddd765063df424d76ac84d41025b4aa982fef1d8
MatthewPun06 <matthewpun06@gmail.com>
2025-03-29 16:57:56 -0400

    Create Code.js

---
+++ Code.js
@@ -0,0 +1,32 @@
+const html = HtmlService.createHtmlOutputFromFile('SideBar.html')
+async function testing() {
+    var doc = DocumentApp.getActiveDocument();
+    var body = doc.getBody();
+    const prompt = 'The most important aspects of a persuasive presentation are:';
+    
+    text = await promptAI(prompt);
+    body.appendParagraph(text);
+}
+
+async function promptAI(prompt) {
+    const gemini = new GeminiApp('AIzaSyBh0INs-TpAoxwRLPIEQy2IZLrcgimpDqI');
+    const model = gemini.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    return response.text();
+
+}
+
+function onOpen() {
+    var ui = DocumentApp.getUi();
+    ui.createMenu('My Add-on')
+      .addItem('use ReWrite', 'openSideBar')
+      .addToUi();
+}
+function openSideBar() {
+      DocumentApp.getUi().showSidebar(html);
+}
+
+async function rateOutOfTen() {
+  
+}
